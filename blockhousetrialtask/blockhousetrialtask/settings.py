bebdb73206818000c098a658fa4af05c074f7757
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-10-18 09:40:20 -0400

    Update installed apps path

--- blockhousetrialtask/blockhousetrialtask/settings.py
+++ blockhousetrialtask/blockhousetrialtask/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fetchfinancialdata.apps.FetchfinancialdataConfig',
+    'blockhousetrialtask.fetchfinancialdata',
 ]
 
 MIDDLEWARE = [
