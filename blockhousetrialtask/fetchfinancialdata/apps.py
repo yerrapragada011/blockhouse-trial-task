2bd0727b927daf42cfae0225c55f10bb682f5df3
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-10-18 10:06:30 -0400

    Fix app config file path

--- blockhousetrialtask/fetchfinancialdata/apps.py
+++ blockhousetrialtask/fetchfinancialdata/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FetchfinancialdataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fetchfinancialdata'
+    name = 'blockhousetrialtask.fetchfinancialdata'
